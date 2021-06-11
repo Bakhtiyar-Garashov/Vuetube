862edd06c6d85c38290d86e80c53d345d5b71e6d
Bakhtiyar Garashov <bakhtiyar.garashov@finceptiv.com>
2021-06-11 23:22:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+from starlette.requests import Request
+
+app = FastAPI()
+
+
+# a response custom type
+class Response(BaseModel):
+    message: str
+    slug: Optional[str]
+    query_param: Optional[str]
+
+
+@app.get('/')
+async def home(request: Request):
+    return {"message": "Yeaahh,it worked"}
+
+
+@app.post('/{slug}')
+async def index(request: Request, slug: str, q: Optional[str]) -> Response:
+    data = Response(message="Salam", slug=slug, query_param=q)
+    return data
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
