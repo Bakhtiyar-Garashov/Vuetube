fdefbb78bcb67ce470c48b2102bd4cfb67d477a3
Bakhtiyar Garashov <bakhtiyar.garashov@finceptiv.com>
2021-06-23 14:31:23 +0300

    Finish API

--- server/main.py
+++ server/main.py
@@ -1,29 +1,39 @@
 from typing import Optional
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
 from starlette.requests import Request
+from pytube import YouTube
+import time
 
 app = FastAPI(title="First project", version="0.0.1")
-
-
-# a response custom type
-class Response(BaseModel):
-    message: str
-    slug: Optional[str]
-    query_param: Optional[str]
-
 
 @app.get('/')
 async def home(request: Request):
     return {"message": "Yeaahh,it worked"}
 
-
-@app.post('/{slug}')
-async def index(request: Request, slug: str, q: Optional[str]) -> Response:
-    data = Response(message="Salam", slug=slug, query_param=q)
-    return data
+@app.get('/find/')
+async def index(url: str):
+    yt = YouTube(url)
+    if yt:
+        videos = yt.streams.filter(progressive=True);
+        return {
+            "info":{
+                'title':yt.title,
+                'author':yt.author,
+                'thumbnail':yt.thumbnail_url,
+                'description':yt.description,
+                'duration':time.strftime('%H:%M:%S',time.gmtime(yt.length)),
+                'views_count':yt.views,
+                'published_date':yt.publish_date,
+                'download_options': [{'url':video.url,'quality':video.resolution} for video in videos]
+             }   
+        }
+    return {
+        'info':{
+            'message':'Video data not found'
+        }
+    }
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=7500)
