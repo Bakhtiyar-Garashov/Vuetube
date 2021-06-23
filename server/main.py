d67dfadddf6dd5b34aa66c64d128fce4460cacf9
Bakhtiyar Garashov <bakhtiyar.garashov@finceptiv.com>
2021-06-23 14:33:42 +0300

    Add cors headers

--- server/main.py
+++ server/main.py
@@ -4,8 +4,22 @@ import uvicorn
 from starlette.requests import Request
 from pytube import YouTube
 import time
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="First project", version="0.0.1")
+
+origins = [
+    "http://localhost:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+)
+
 
 @app.get('/')
 async def home(request: Request):
